1c1443aba4ba338ca504e1f4eed6108b79bf81fc
jiang263 <2635673264@qq.com>
2020-05-14 04:18:09 +0000

    Deploying to gh-pages from master c51283ca15de15998acc71bd68588e5db6206a46

---
+++ precache-manifest.b8f928151fadd1fe52e9c68ec94e0902.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1fcc439920fbff08b0",
+    "url": "/vue-action-delpoy/css/app.dcdece68.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-action-delpoy/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "72c16fd5aa1bdbc31afe9391862a80c5",
+    "url": "/vue-action-delpoy/index.html"
+  },
+  {
+    "revision": "c0e57553f77281901de1",
+    "url": "/vue-action-delpoy/js/about.69c2a69c.js"
+  },
+  {
+    "revision": "3c1fcc439920fbff08b0",
+    "url": "/vue-action-delpoy/js/app.095b5fba.js"
+  },
+  {
+    "revision": "1fbc43188f1d193856f6",
+    "url": "/vue-action-delpoy/js/chunk-vendors.906225ea.js"
+  },
+  {
+    "revision": "960901be4c6c64518d51776d8ce8762f",
+    "url": "/vue-action-delpoy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-action-delpoy/robots.txt"
+  }
+]);
